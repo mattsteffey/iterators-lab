304d890ee39d2488f89ecf1f26dc15c984aac79a
mattsteffey <33659379+mattsteffey@users.noreply.github.com>
2017-12-10 11:30:40 -0700

    Add files via upload

--- myReduce.js
+++ myReduce.js
@@ -1,22 +1,27 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
+  
+  var i=0; 
+  var variable;
 
-//  CODE INSIDE HERE   //
+  if(typeof(initialValue) == "undefined") {
+    i++;
+    variable = arr[0];
+  } else {
+    variable = initialValue;
+  }
 
+  for (; i<arr.length; i++) {
+    variable = callback(variable, arr[i], i, arr);
+  }
+  return variable;
 }
-
-/*
- Best if you don't code out here.
- If you want to check your code, use `index.js`!
-*/
-
-
-
-
 
 
 
 // export this function (you can ignore this for now)
 module.exports = myReduce;
+
+
